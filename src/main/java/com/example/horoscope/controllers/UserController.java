61f6f7c33044efa653784b5838b41bfa8bdfb9a2
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-05-19 22:44:15 -0300

    create connection with database

--- src/main/java/com/example/horoscope/controllers/UserController.java
+++ src/main/java/com/example/horoscope/controllers/UserController.java
@@ -1,6 +1,11 @@
 package com.example.horoscope.controllers;
 
+import com.example.horoscope.model.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    @GetMapping("")
-    public String helloWorld(){
-        return "Hello World";
+    @GetMapping("{id}")
+    public User user(@PathVariable("id") int id){
+        System.out.println(id);
+        User user = new User();
+
+        user.setId(1);
+        user.setName("asd23");
+        user.setPassword("aaaa");
+        return user;
+    }
+
+    @PostMapping("")
+    public User user(@RequestBody User user){
+        return user;
     }
 
 }
